fa5455043a052124a423612d38762e24b28bdb77
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-26 01:07:44 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,5 +5,23 @@
 
 class EmailAddressParser 
   attr_accessor :emails 
+  def self.new_from_csv(csv_data)
+   rows = csv_data.split("\n")
+   people = rows.collect do |row|
+     data = row.split(", ")
+     name = data[0]
+     age = data[1]
+     company = data[2]
+ 
+     person = self.new # This is an important line.
+     person.name = name
+     person.age = age
+     person.company = company
+     person
+   end
+   people
+ end
+  
+  
   
 end   
